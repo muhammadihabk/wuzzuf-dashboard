9b4a1163e696f8cf70a4ee4c34db067f8a2814ca
muhammad <69121442+muhammadihabk@users.noreply.github.com>
2023-02-01 14:43:03 +0200

    first commit

---
+++ src/test/java/com/example/wuzzufdashboard/WuzzufDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wuzzufdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuzzufDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
