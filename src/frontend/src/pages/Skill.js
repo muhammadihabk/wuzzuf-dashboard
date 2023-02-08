cae6350e2fb4b62857d04dad3044d62ce9571d8f
muhammad <69121442+muhammadihabk@users.noreply.github.com>
2023-02-08 13:02:49 +0200

    Implemented pagination in skill page

--- src/frontend/src/pages/Skill.js
+++ src/frontend/src/pages/Skill.js
@@ -1,30 +1,52 @@
 import '../css/Skill.css';
-import { React, useEffect, useState } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import JobCardBySkill from '../components/JobCardBySkill';
+import useGetJobsForHome from '../hooks/useGetJobsForHome';
 
 
 export const Skill = () => {
-    const [ jobs, setJobs ] = useState([]);
-    let { skillName } = useParams();
+    const { skillName } = useParams();
+    const [ pageNum, setPageNum ] = useState(0);
 
-    useEffect(() => {
-            const fetchJobs = async() => {
-                if(skillName.toLowerCase() === 'c++') {
-                    skillName = 'c%2B%2B';
-                }
-                const response = await fetch(`http://localhost:8080/app/skill?pageNum=0&filter=${skillName}`);
-                const data = await response.json();
-                setJobs(data);
-            };
-            fetchJobs();
-        },[skillName]
-    );
+    const {
+        jobs,
+        isLoading,
+        isError,
+        errorMsg,
+        hasNextPage
+    } = useGetJobsForHome(skillName, pageNum);
+
+    const observer = useRef();
+    const lastCardRef = useCallback(job => {
+        if(isLoading) { return; }
+        if(observer.current) { observer.current.disconnect(); }
+        observer.current = new IntersectionObserver(observerJobs => {
+            if(observerJobs[0].isIntersecting && hasNextPage) {
+                setPageNum(prev => prev + 1);
+            }
+        });
+        if(job) { observer.current.observe(job); }
+    }, [isLoading, hasNextPage]);
+
+    if(isError) {
+        return <p className='api-error'>Error: {errorMsg}</p>;
+    }
+
+    let cards = [];
+    if(jobs.length != 0) {
+        for(let i = 0; i < jobs.length - 1; i++) {
+            cards.push(<JobCardBySkill key={jobs[i].id} job={jobs[i]}/>);
+        }
+        cards.push(<JobCardBySkill ref={lastCardRef} key={jobs[jobs.length - 1].id} job={jobs[jobs.length - 1]}/>);
+    }
 
     return (
         <div className='Skill'>
             <h1 className='page-title'>{skillName}</h1>
-            <div className='cards'>{jobs.map(job => <JobCardBySkill key={job.id} job={job}/> )}</div>
+            <div className="cards">{cards}</div>
+            {isLoading && <p>loading...</p>}
+            <a className='to-page-top' href='#top'>^</a>
         </div>
     );
 }
