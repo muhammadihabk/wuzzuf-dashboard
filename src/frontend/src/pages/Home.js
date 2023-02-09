95d997fea48dc2ecf987d3561eaa6ef75f7950e6
muhammad <69121442+muhammadihabk@users.noreply.github.com>
2023-02-09 13:27:33 +0200

    Better UX for searchbar

--- src/frontend/src/pages/Home.js
+++ src/frontend/src/pages/Home.js
@@ -47,10 +47,24 @@ export const Home = () => {
         setPageNum(0);
         setFilter(e.target.value);
     };
+
+    const searchbar = document.getElementById('searchbar');
+    window.onkeyup = e => {
+        if(e.key === "/") {
+            searchbar.focus();
+        }
+    };
+
+    const handleSubmit = e => e.preventDefault();
     
     return (
         <div className="Home" id='top'>
-            <input className="searchbar" type="text" placeholder="skill, role or company" onChange={handleSearch}/>
+            <form onSubmit={handleSubmit}>
+                <div className="searchbar">
+                    <span className='searchbar-hint'>/</span>
+                    <input className="searchbar__input" id='searchbar' type="text" placeholder="skill, role or company" onChange={handleSearch}/>
+                </div>
+            </form>
             <div className="cards">{cards}</div>
             {isLoading && <p>loading...</p>}
             <a className='to-page-top' href='#top'>^</a>
