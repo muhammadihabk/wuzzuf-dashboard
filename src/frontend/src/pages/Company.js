d98b7b03558889dc30243b702ad5e7024cf2f200
muhammad <69121442+muhammadihabk@users.noreply.github.com>
2023-02-10 16:52:01 +0200

    Bug fix in infinite scroll
    fixed the condition of the observer

--- src/frontend/src/pages/Company.js
+++ src/frontend/src/pages/Company.js
@@ -1,4 +1,4 @@
-import '../css/Company.css'
+import '../css/Company.css';
 import { useState, useRef, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import JobCardByCompany from '../components/JobCardByCompany';
@@ -8,7 +8,7 @@ import useGetJobs from '../hooks/useGetJobs';
 export const Company = () => {
     const { companyName } = useParams();
     const [ pageNum, setPageNum ] = useState(0);
-
+    
     const {
         jobs,
         isLoading,
@@ -22,7 +22,7 @@ export const Company = () => {
         if(isLoading) { return; }
         if(observer.current) { observer.current.disconnect(); }
         observer.current = new IntersectionObserver(observerJobs => {
-            if(observerJobs[0].isIntersecting && hasNextPage && pageNum >= 12) {
+            if(observerJobs[0].isIntersecting && hasNextPage && jobs.length >= 12) {
                 setPageNum(prev => prev + 1);
             }
         });
@@ -34,14 +34,14 @@ export const Company = () => {
     }
 
     let cards = [];
-    if(jobs.length != 0) {
+    if(jobs.length !== 0) {
         for(let i = 0; i < jobs.length - 1; i++) {
             cards.push(<JobCardByCompany key={jobs[i].id} job={jobs[i]} setPageNum={setPageNum}/>);
         }
         cards.push(<JobCardByCompany ref={lastCardRef} key={jobs[jobs.length - 1].id} job={jobs[jobs.length - 1]}
             setPageNum={setPageNum}/>);
     }
-    
+
     return (
         <div className='Company'>
             <h1 className='page-title'>{companyName}</h1>
